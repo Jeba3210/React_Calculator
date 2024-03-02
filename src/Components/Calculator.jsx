73d0c95943abdcae16d3c2f7de81860ef315e164
Jeba3210 <jebaparvin2018@gmail.com>
2024-03-02 12:05:00 +0530

    Refresh button added

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -21,7 +21,6 @@ function Calculator() {
       setCalculatedValue((+value1) + (+value2));
     }
 
-
     function implementSubtraction(e){
       e.preventDefault();
         if(!value1 || !value2){
@@ -62,7 +61,7 @@ function Calculator() {
   return (
     <div className="h-screen flex justify-center place-items-center content-center bg-gradient-to-b 
     from-green-200  to-green-500">
-        <div className="grid  place-items-center text-center h-1/2 w-2/5 m-auto rounded-xl  bg-gradient-to-b 
+        <div className="grid  place-items-center text-center h-3/5 w-2/5 m-auto rounded-xl  bg-gradient-to-b 
                  from-green-100 to-green-200 content-center">
         <div>
           <h2 className='text-5xl font-bold text-red-400'>React Calculator</h2>
@@ -93,29 +92,29 @@ function Calculator() {
           <div className="buttons mt-6 ml-10
            flex flex-row">
             <div>
-                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md' onClick={implementAddition}><span>&#43;</span></button>
+                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md  font-bold text-xl text-slate-100' onClick={implementAddition}><span>&#43;</span></button>
             </div>
             <div>
-                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md' onClick={implementSubtraction}><span>&#8722;</span></button>
+                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md  font-bold text-xl text-slate-100' onClick={implementSubtraction}><span>&#8722;</span></button>
             </div>
             <div>
-                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md' onClick={implementMultiplication}><span>&#215;</span></button>
+                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md  font-bold text-xl text-slate-100' onClick={implementMultiplication}><span>&#215;</span></button>
             </div>
             <div>
-                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md' onClick={implementDivision}><span>&#247;</span></button>
+                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md  font-bold text-xl text-slate-100' onClick={implementDivision}><span>&#247;</span></button>
             </div>
             <div>
-                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md' onClick={implementModulo}><span>&#37;</span></button>
+                <button type='submit' className='h-10 w-11 bg-green-400 mr-10 rounded-md  font-bold text-xl text-slate-100' onClick={implementModulo}><span>&#37;</span></button>
             </div>
           </div>
-
-             {/* <button type='Submit' onClick={implementCalculation}></button> */}
+           <div className='mt-8 flex justify-center place-items-center h-10 w-40 bg-green-400 text-slate-200 rounded-lg  font-bold text-xl ml-36'> <button>Reset<i className="material-icons">&#8634;</i></button></div>
 
              
         </form>
         
-
-        <div className='mt-10 flex justify-center place-items-center h-14 w-80 bg-red-400 text-slate-100 rounded-lg  font-bold text-2xl'>Result : {calculatedValue}</div>
+        {
+           <div className='mt-10 flex justify-center place-items-center h-14 w-80 bg-red-400 text-slate-100 rounded-lg  font-bold text-2xl'>Result : {calculatedValue}</div>
+        }
         </div>
         
        
